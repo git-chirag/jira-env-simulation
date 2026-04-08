7391c777d2aff8f6a4f76dfd70167a4cd3e68f58
Chirag Aparadh <chiragaparadh@gmail.com>
2026-04-08 19:01:12 +0530

    fix: expose root-level graders for validator discovery

---
+++ graders.py
@@ -0,0 +1,10 @@
+from tasks.graders import TASK_GRADERS, grade_action, grade_easy, grade_hard, grade_medium
+
+
+__all__ = [
+    "grade_easy",
+    "grade_medium",
+    "grade_hard",
+    "grade_action",
+    "TASK_GRADERS",
+]
